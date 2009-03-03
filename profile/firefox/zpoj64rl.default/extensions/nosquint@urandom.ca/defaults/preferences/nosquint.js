6c3bde3f8f73db2c78d8038fba032f3a48667f3b
Tom Bruno <tebruno99@gmail.com>
2009-03-03 13:14:50 -0600

    Added No-Squint plugin to remember sites zoom levels

---
+++ profile/firefox/zpoj64rl.default/extensions/nosquint@urandom.ca/defaults/preferences/nosquint.js
@@ -0,0 +1,10 @@
+pref("extensions.nosquint.zoomlevel", 120);
+pref("extensions.nosquint.zoomIncrement", 10);
+pref("extensions.nosquint.rememberSites", true);
+pref("extensions.nosquint.sites", "");
+pref("extensions.nosquint.sitesSaveDelay", 5000);
+pref("extensions.nosquint.exceptions", "*/~* *.sourceforge.net *.google.[*]");
+pref("extensions.nosquint.wheelZoomEnabled", false);
+pref("extensions.nosquint.hideStatus", false);
+pref("extensions.nosquint.forgetMonths", 6);
+pref("extensions.nosquint.fullZoomPrimary", true);
